14c60e905a77541051d991d87f254d76e140a96b
jamesob131 <oboyle.james13@gmail.com>
2019-08-27 16:03:16 -0400

    first commit, adding existing files

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const express = require('mongoose')
+const express = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// 
