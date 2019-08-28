0e7f8d95c82ae671ec4eb521852e514ed5226d7f
jamesob131 <oboyle.james13@gmail.com>
2019-08-28 12:13:19 -0400

    added connnection string for db and item shcema

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const express = require('mongoose')
-const express = require('body-parser');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
-// 
+// Db Congif
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongodb using mongoose 
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => console.log(`Server started on port ${port}`));
