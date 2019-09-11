fc1c626e77b2eb2fbf36807e81782da49cf25ceb
jamesob131 <oboyle.james13@gmail.com>
2019-09-11 15:31:26 -0400

    implemented most of redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -9,10 +12,12 @@ import './App.css';
 class App extends Component {
   render(){  
     return (
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+        </Provider>
       );
   }
 }
