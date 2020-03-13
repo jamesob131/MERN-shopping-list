4b0d5d304da6eed856e7840b93dbef49ec104a85
jamesob131 <oboyle.james13@gmail.com>
2020-03-12 22:29:58 -0400

    adding jwt auth

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://jamesob131:Seanob13@mern-shopping-i2uma.mongodb.net/test?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
 const path = require('path');
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Db Congif
+// Db Config
 
-const db = require('./config/keys').mongoURI;
+
+const db = config.get('mongoURI');
 
 // Connect to mongodb using mongoose 
 mongoose
-    .connect(db, {useNewUrlParser: true})
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);  
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users')); 
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
